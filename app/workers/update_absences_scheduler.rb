800d8e59472d02dc4883debfe35bb1244eb569e2
Louis Davin <louis.developpement@gmail.com>
2015-09-13 12:29:50 +0200

    Deactivate grades and absences updates on weekends, deactivate schedule updates on sundays

--- app/workers/update_absences_scheduler.rb
+++ app/workers/update_absences_scheduler.rb
@@ -1,11 +1,15 @@
 class UpdateAbsencesScheduler
 
   def perform
-    User.includes(:updates).find_each do |user|
-      if user.user_ok? and user.main_session.present? and user.is_eligible_for_absences_or_grades_update?
-        Delayed::Job.enqueue FetchAbsencesWorker.new(user.id, user.main_session.id),
-                             priority: ApplicationWorker::PR_FETCH_ABSENCES,
-                             queue: ApplicationWorker::QUEUE_REGULAR
+    wday = Date.today.wday
+    # Only update absences if not on weekend
+    if wday != 6 && wday != 0
+      User.includes(:updates).find_each do |user|
+        if user.user_ok? and user.main_session.present? and user.is_eligible_for_absences_or_grades_update?
+          Delayed::Job.enqueue FetchAbsencesWorker.new(user.id, user.main_session.id),
+                               priority: ApplicationWorker::PR_FETCH_ABSENCES,
+                               queue: ApplicationWorker::QUEUE_REGULAR
+        end
       end
     end
   end
--- app/workers/update_grades_scheduler.rb
+++ app/workers/update_grades_scheduler.rb
@@ -1,11 +1,15 @@
 class UpdateGradesScheduler
 
   def perform
-    User.includes(:updates).find_each do |user|
-      if user.user_ok? and user.main_session.present? and user.is_eligible_for_absences_or_grades_update?
-        Delayed::Job.enqueue FetchDetailedGradesWorker.new(user.id, user.main_session.id),
-                             priority: ApplicationWorker::PR_FETCH_DETAILED_GRADES,
-                             queue: ApplicationWorker::QUEUE_DETAILED_GRADES
+    wday = Date.today.wday
+    # Only update grades if not on weekend
+    if wday != 6 && wday != 0
+      User.includes(:updates).find_each do |user|
+        if user.user_ok? and user.main_session.present? and user.is_eligible_for_absences_or_grades_update?
+          Delayed::Job.enqueue FetchDetailedGradesWorker.new(user.id, user.main_session.id),
+                               priority: ApplicationWorker::PR_FETCH_DETAILED_GRADES,
+                               queue: ApplicationWorker::QUEUE_DETAILED_GRADES
+        end
       end
     end
   end
